a4f7786390f83ad1adf7336b0db95114e5b3c19a
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-07-24 14:58:09 +0330

    Refactor gameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,9 +17,9 @@ const GameGrid = ({gameQuery}: Props) => {
     const {data, error, isLoading} = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+    if (error) return <Text>{error}</Text>;
+
     return (
-        <>
-            {error && <Text className="text-danger">{error}</Text>}
             <SimpleGrid
                 columns={{sm: 1, md: 2, lg: 3, xl: 4}}
                 padding="10px"
@@ -36,7 +36,6 @@ const GameGrid = ({gameQuery}: Props) => {
                     </GameCardContainer>
                 ))}
             </SimpleGrid>
-        </>
     );
 };
 
