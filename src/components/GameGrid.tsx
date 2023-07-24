1f863801633cb643b53002b46cd7608a2438d120
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-07-24 13:54:44 +0330

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -23,7 +23,7 @@ const GameGrid = ({gameQuery}: Props) => {
             <SimpleGrid
                 columns={{sm: 1, md: 2, lg: 3, xl: 4}}
                 padding="10px"
-                spacing={3}
+                spacing={6}
             >
                 {isLoading &&
                     skeletons.map(skeleton => (
