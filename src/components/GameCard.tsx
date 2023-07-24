cc0d90da1b2756898cd399c384459042ad75e28f
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-07-25 00:07:28 +0330

    Refactor style GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({game}: Props) => {
     return (
-        <Card>
+        <Card height="100%">
             <Image src={getCroppedImageUrl(game.background_image)}/>
             <CardBody>
                 <HStack justifyContent="space-between" marginBottom={3}>
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import {Card, CardBody, Skeleton, SkeletonText} from "@chakra-ui/react";
 const GameCardSkeleton = () => {
     return (
         <Card >
-            <Skeleton height="200px" />
+            <Skeleton height="250px" />
             <CardBody>
                 <SkeletonText />
             </CardBody>
